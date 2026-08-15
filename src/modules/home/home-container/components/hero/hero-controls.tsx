8db667d3d387b8e5721a7d4bc2006edf1aa8d73c
nhoang123 <hn9592336@gmail.com>
2026-08-15 13:42:34 +0700

    update code

--- src/modules/home/home-container/components/hero/hero-controls.tsx
+++ src/modules/home/home-container/components/hero/hero-controls.tsx
@@ -13,7 +13,7 @@ export function HeroControls({ onPrev, onNext }: Props) {
       <Button
         onClick={onPrev}
         aria-label='Xem slide trước'
-        className='group hero-prev-btn absolute left-[1rem] xsm:left-[0.5rem] top-1/2 z-30 flex h-[3rem] xsm:h-[2rem] w-[3rem] xsm:w-[2rem] -translate-y-1/2 items-center justify-center rounded-full border border-white/70 bg-[#e1e5e7] shadow-md transition-colors hover:bg-[#51c6f5]'
+        className='group hero-prev-btn absolute left-[1rem] xsm:left-[0.5rem] top-1/2 z-30 flex h-[3rem] xsm:h-[2rem] w-[3rem] xsm:w-[2rem] -translate-y-1/2 items-center justify-center rounded-full border border-white/70 bg-[#e1e5e7] shadow-md transition-colors hover:bg-[#a3a9ac]'
       >
         <ChevronLeft className='h-[1.25rem] w-[1.25rem] xsm:h-[1rem] xsm:w-[1rem] text-gray-500 group-hover:text-[#dee6e9]' />
       </Button>
@@ -21,7 +21,7 @@ export function HeroControls({ onPrev, onNext }: Props) {
       <Button
         onClick={onNext}
         aria-label='Xem slide tiếp theo'
-        className='group hero-next-btn absolute right-[1rem] xsm:right-[0.5rem] top-1/2 z-30 flex h-[3rem] xsm:h-[2rem] w-[3rem] xsm:w-[2rem] -translate-y-1/2 items-center justify-center rounded-full border border-white/70 bg-[#e1e5e7] shadow-md transition-colors hover:bg-[#51c6f5]'
+        className='group hero-next-btn absolute right-[1rem] xsm:right-[0.5rem] top-1/2 z-30 flex h-[3rem] xsm:h-[2rem] w-[3rem] xsm:w-[2rem] -translate-y-1/2 items-center justify-center rounded-full border border-white/70 bg-[#e1e5e7] shadow-md transition-colors hover:bg-[#a3a9ac]'
       >
         <ChevronRight className='h-[1.25rem] w-[1.25rem] xsm:h-[1rem] xsm:w-[1rem] text-gray-500 group-hover:text-[#dee6e9]' />
       </Button>
